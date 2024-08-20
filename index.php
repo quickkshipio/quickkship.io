18e62355292283bb18398525c10ab8702aed55eb
quickkshipio <167225308+quickkshipio@users.noreply.github.com>
2024-08-20 16:11:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,147 +1,85 @@
-<?php include('header.php'); ?>
-
-<div class="main-banner" style="background-image: url('jobado/layout/images/d.jpg'); padding: 225px 0px;">
-    <div class="container">
-        <div class="caption header-text">
-            <div class="custom-box" style="margin-left: 0;">
-                <h2 class="about-heading mb_40 wow fadeInUp" style="font-size: xx-large; line-height: 70px;" data-wow-delay="0.2s">Track your shipment</h2>
-                <p>Track your shipment with ease! Enter your unique Tracking ID below and click 'Track Now' for real-time updates on your delivery. Stay informed about the status, location, and estimated delivery time. We're committed to providing accurate and timely tracking details to enhance your experience. Thank you for choosing our services; we're here to keep you informed every step of the way!</p>
+<!DOCTYPE html>
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <!-- =====  BASIC PAGE NEEDS  ===== -->
+  <title><?php if($pgTitle){echo $pgTitle;} ?>QuickkShip</title>
+	<link rel="canonical" href="https://html.lionode.com" />
+  <!-- =====  SEO MATE  ===== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="distribution" content="global">
+  <meta name="revisit-after" content="2 Days">
+  <meta name="robots" content="ALL">
+  <meta name="rating" content="8 YEARS">
+  <meta name="Language" content="en-us">
+  <meta name="GOOGLEBOT" content="NOARCHIVE">
+  <!-- =====  MOBILE SPECIFICATION  ===== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="viewport" content="width=device-width">
+  <!-- =====  CSS  ===== -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/font-awesome.min.css" />
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/menu.css">
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/style.css">
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/new-style.css">
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/color/style1.css" id="colors">
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/animation.css">
+  <link rel="stylesheet" type="text/css" href="jobado/layout/css/owl.carousel.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link rel="shortcut icon" href="jobado/layout/images/favicon.png">
+  <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  
+</head>
+<body id="home" style="background-color: #141416;">
+  <div class="wrapper" style="background-color: #141416;">
+    
+    <?php include('header.php'); ?>
+    
+    <!-- =====  HEADER END  ===== -->
+    <!-- =====  BANNER STRAT  ===== -->
+    <div class="banner2 large-header bg-color" id="large-header">
+      <div class="banner-detail ">
+        <div class="cd-intro">
+          <div class="custom-box">
+            <h2 class="about-heading mb_40 wow fadeInUp" data-wow-delay="0.2s">Welcome to QuickkShip</h2>
+            <div class="text">
+              QuickkShip is revolutionizing the way you ship packages. Our innovative platform offers seamless solutions for individuals and businesses alike. Say goodbye to shipping hassles and hello to convenience with QuickkShip.
             </div>
-            <div class="search-input" style="padding-top: 30px;">
-                <form id="searchForm" class="searchForm" action="#">
-                    <input type="text" placeholder="Tracking ID" class="searchText" id="searchText" name="searchKeyword" />
-                    <button type="submit" style="background-color: #ffc107;">Track Now</button>
-                </form>
-            </div>
+              <a class="custom-btn custom-btns light mtb_30 wow fadeInUp" data-wow-delay="0.2s" href="#about" id="scrollToAbout"><span>Read more</span></a>
+          </div>
+          
+          <script>
+              document.getElementById("scrollToAbout").addEventListener("click", function (event) {
+                  event.preventDefault(); // Prevent the default behavior of jumping to the section
+                  const target = document.querySelector("#about");
+                  if (target) {
+                      target.scrollIntoView({ behavior: "smooth" }); // Use smooth scroll behavior
+                  }
+              });
+          </script>
         </div>
+      </div>
     </div>
-</div>
-<!-- =====  ABOUT END  ===== -->
-
-<section id="about" style="background-color: #141416;" class="ptb_100">
-    <div class="about">
+    <!-- =====  BANNER END  ===== -->
+    <section id="about" style="background-color: #141416;" class="ptb_100">
+      <div class="about">
         <div class="container">
-            <div class="about-inner ">
-                <div class="row">
-                    <div class="col-md-16">
-                        <div class="about-text">
-                            <div class="about-heading-wrap">
-                                <h2 class="about-heading mb_40 wow fadeInUp" data-wow-delay="0.2s">Track your shipment</h2>
-                            </div>
-                            <p>Track your shipment with ease! Enter your unique Tracking ID below and click 'Track Now' for real-time updates on your delivery. Stay informed about the status, location, and estimated delivery time. We're committed to providing accurate and timely tracking details to enhance your experience. Thank you for choosing our services; we're here to keep you informed every step of the way!.</p>
-                        </div>
-                    </div>
-                    <div class="main-banner">
-                        <div class="caption header-text">
-                            <div class="search-input" style="padding-left: 20px; padding-right: 20px;">
-                                <form id="searchForm" action="#">
-                                    <input type="text" placeholder="Tracking ID" id="searchText" name="searchKeyword" />
-                                    <button type="submit" style="background-color: #ffc107;">Track Now</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- =====  FEATURES START  ===== -->
-<section id="about" class="ptb_100">
-    <div class="about">
-        <div class="container">
-            <div class="about-inner ">
-                <div class="row">
-                    <div class="col-md-7">
-                        <div class="about-text">
-                            <div class="about-heading-wrap">
-                                <h2 class="about-heading mb_40 wow fadeInUp" data-wow-delay="0.2s">About QuickkShip</h2>
-                            </div>
-                            <p class="wow fadeInUp" data-wow-delay="0.3s">QuickkShip is a premier shipment company based in the bustling city of New York, offering unparalleled delivery services with a focus on safety, speed, and transparency. Our mission is simple: to provide customers with peace of mind when it comes to shipping their most valuable items, regardless of packaging, size, or fragility..</p>
-                            <a class="custom-btn custom-btns light mtb_30 wow fadeInUp" data-wow-delay="0.4s" type="button" href="about.html"><span>View features</span></a>
-                            <script>
-                                document.getElementById("scrollToPortfolio").addEventListener("click", function (event) {
-                                    event.preventDefault(); // Prevent the default behavior of jumping to the section
-                                    const target = document.querySelector("#portfolio");
-                                    if (target) {
-                                        target.scrollIntoView({ behavior: "smooth" }); // Use smooth scroll behavior
-                                    }
-                                });
-                            </script>
-                        </div>
-                    </div>
-                    <div class="col-md-5 wow fadeInUp" data-wow-delay="0.2s">
-                        <figure> <img src="jobado/layout/images/profilepic-2.jpg" alt="#"> </figure>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- =====  HOW IT WORKS START  ===== -->
-<section id="about" style="background-color: #141416;" class="ptb_100">
-    <div class="container pb_100">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center">
-                <div class="heading-part">
-                    <h2 class="about-heading mb_40 wow fadeInUp" data-wow-delay="0.2s">How It Works</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row text-left">
-            <div class="col-sm-16 mt_80">
-                <div class="feature-i-left wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="icon-right"><i class="icon ion-easel"></i></div>
-                    <h4 class="mb_10 title">1- Sign Up</h4>
-                    <p>Create your QuickkShip account in minutes. It’s fast, easy, and free!</p>
-                </div>
-            </div>
-            <div class="col-sm-16 mt_80">
-                <div class="feature-i-left wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="icon-right"><i class="icon ion-easel"></i></div>
-                    <h4 class="mb_10 title">2- Schedule shipment</h4>
-                    <p>Input your shipment details, including pickup location, delivery address, and package dimensions.</p>
-                </div>
-            </div>
-            <div class="col-sm-16 mt_80">
-                <div class="feature-i-left wow fadeInUp" data-wow-delay="0.4s">
-                    <div class="icon-right"><i class="icon ion-easel"></i></div>
-                    <h4 class="mb_10 title">3- Choose Shipping Option</h4>
-                    <p>Select from a variety of shipping options, including delivery speed and package insurance.</p>
-                </div>
-            </div>
-            <div class="col-sm-16 mt_80">
-                <div class="feature-i-left wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="icon-right"><i class="icon ion-easel"></i></div>
-                    <h4 class="mb_10 title">4- Track Package</h4>
-                    <p>Keep tabs on your package’s journey with real-time tracking updates and direct driver communication.</p>
-                </div>
-            </div>
-            <div class="col-sm-16 mt_80">
-                <div class="feature-i-left wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="icon-right"><i class="icon ion-easel"></i></div>
-                    <h4 class="mb_10 title">5- Receive Confirmation</h4>
-                    <p>Once your package is delivered, receive confirmation and peace of mind.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- =====  FOOTER START  ===== -->
-<?php include('footer.php'); ?>
-
-<script type="module">
-    document.getElementById("searchForm").addEventListener("submit", function (e) {
-        e.preventDefault(); // Prevents the default form submission
-
-        const searchText = document.getElementById('searchText').value;
-        const trackingPageURL = `tracking.html?trackingId=${encodeURIComponent(searchText)}`;
-
-        window.location.href = trackingPageURL;
-    });
-</script>
-
-<!-- =====  FOOTER END  ===== -->
+          <div class="about-inner ">
+            <div class="row">
+              <div class="col-md-16">
+                <div class="about-text">
+                  <div class="about-heading-wrap">
+                    <h2 class="about-heading mb_40 wow fadeInUp" data-wow-delay="0.2s">Track your shipment</h2>
+                  </div>
+                  <p>Track your shipment with ease! Enter your unique Tracking ID below and click 'Track Now' for real-time updates on your delivery. Stay informed about the status, location, and estimated delivery time. We're committed to providing accurate and timely tracking details to enhance your experience. Thank you for choosing our services; we're here to keep you informed
